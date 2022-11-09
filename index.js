8f2df85695c20a6150825eced452984656ce70d6
KaiqiGeorge <oliviakaiqihuang@gmail.com>
2022-11-09 16:33:27 -0700

    added the js part of this project

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-let inputs=document.querySelectorAll(".control input");
+let inputs = document.querySelectorAll(".control input");
+function handleUpdate() {
+  console.log(this);
+  let unit = this.dataset.unit || "";
+  document.documentElement.style.setProperty(
+    `--${this.name}`,
+    this.value + unit
+  );
+}
+
+inputs.forEach(input => input.addEventListener("change", handleUpdate));
+inputs.forEach(input => input.addEventListener("mousemove", handleUpdate));
